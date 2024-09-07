39a3fd2b7c88e231d5c6b7536908ea87beb6e649
Gorisha2004 <157053342+Gorisha2004@users.noreply.github.com>
2024-09-07 12:41:06 +0530

    Create RewardSystem.sol

---
+++ RewardSystem.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "hardhat/console.sol";
+
+contract Reward is ERC20, Ownable, ERC20Burnable {
+
+    // Event emitted when a player redeems tokens for an event or item
+    event Redeem(address indexed player, uint256 amount);
+
+    constructor() ERC20("Reward", "RWD") Ownable(msg.sender) {
+        // Initial mint for the owner (could be used for initial reward )
+        _mint(msg.sender, 10000 * 10 ** decimals());
+    }
+        // Mint tokens to reward players for participating in events (only owner can mint)
+        function mint(address to, uint256 amount) external onlyOwner {
+            _mint(to, amount);
+    }
+
+    function getbalance() external view returns(uint256){
+        return this.balanceOf(msg.sender);
+    }
+     // Players can transfer tokens to other players
+    function transferToken(address _receiver, uint256 _value) external {
+        require(balanceOf(msg.sender) >= _value, "You do not have enough Degen Tokens ");
+        approve(msg.sender, _value);
+        transferFrom(msg.sender, _receiver, _value);
+    }
+
+    // Players redeem tokens for in-game rewards 
+    function purchaseItem(uint256 amount) public {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance to redeem tokens");
+        _burn(msg.sender, amount);
+        emit Redeem(msg.sender, amount);
+    }
+
+    // Players can burn their own tokens if they no longer need them
+    function burnTokens(uint256 amount) public {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance to burn tokens");
+        _burn(msg.sender, amount);
+    }
+}
